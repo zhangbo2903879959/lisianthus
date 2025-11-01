38224b5813e3669310a14f6245d17b0c07810214
zhangbo <2903879959@qq.com>
2025-11-02 01:10:39 +0800

    Merge remote changes

---
+++ src/main/java/com/example/yourapp/lisiathusStart.java
@@ -0,0 +1,11 @@
+package com.example.yourapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//引导类
+@SpringBootApplication
+public class lisiathusStart {
+    public static void main(String[] args) {
+        SpringApplication.run(lisiathusStart.class, args);
+    }
+
+}
